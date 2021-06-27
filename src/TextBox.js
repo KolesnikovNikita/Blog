622e6c6862a55978cca41351624561c2d2e68180
Kolesnikov_Nikita <nik_kolesnikov_1988@ukr.net>
2021-06-27 17:54:35 +0300

    Deleted garbage from TextBox

--- src/TextBox.js
+++ src/TextBox.js
@@ -3,7 +3,6 @@ import React from 'react';
 const TextBox = (props) => {
   return <div>
     <span>Inside we are all the same! {props.text} <br /> {props.author}</span> <br />
-    <span>{props.created}<br />{props.updated}</span>
   </div>
 }
 
